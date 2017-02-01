964fde1358014ada1af30d5e80fd8aa487dcb823
Alec Thomson <alect@mit.edu>
2017-02-01 16:36:45 -0500

    Made a hexagon that goes UP!

---
+++ Assets/Scripts/GameObjects/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public float moveSpeed = 10;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.UpArrow)) {
+			transform.position = new Vector3(transform.position.x, 
+				transform.position.y + moveSpeed*Time.deltaTime,
+				transform.position.z);
+		}
+	}
+}
